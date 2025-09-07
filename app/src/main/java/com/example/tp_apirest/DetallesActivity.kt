4f80eede775a88c57b0fb1f0774fca52a55e6319
Ramiro <ciesielzykramiro@gmail.com>
2025-09-06 22:11:02 -0300

    Desarrollo de la vista y logica de la activity detalles, con la implementacion de un ImageView y un botón que te envía fuera de la App para reproducir contenido.

---
+++ app/src/main/java/com/example/tp_apirest/DetallesActivity.kt
@@ -0,0 +1,95 @@
+package com.example.tp_apirest
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetallesActivity : AppCompatActivity() {
+
+    lateinit var ivPortada: ImageView
+    lateinit var tvTitulo: TextView
+    lateinit var tvAutor: TextView
+    lateinit var tvDuracion: TextView
+    lateinit var tvFecha: TextView
+    lateinit var tvDescripcion: TextView
+    lateinit var btnReproducir: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalles)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        ivPortada = findViewById(R.id.ivPortada)
+        tvTitulo = findViewById(R.id.tvTitulo)
+        tvAutor = findViewById(R.id.tvAutor)
+        tvDuracion = findViewById(R.id.tvDuracion)
+        tvFecha = findViewById(R.id.tvFecha)
+        tvDescripcion = findViewById(R.id.tvDescripcion)
+        btnReproducir = findViewById(R.id.btnReproducir)
+
+        val bundle: Bundle? = intent.extras
+        if(bundle != null){
+            tvTitulo.text = resources.getString(R.string.titulo) + " " + bundle.getString("TITULO")
+            tvAutor.text = resources.getString(R.string.autor) + " " + bundle.getString("AUTOR")
+            tvDuracion.text = resources.getString(R.string.duracion) + " " + bundle.getString("DURACION")
+            tvFecha.text = resources.getString(R.string.fecha_lanzamiento) + " " + bundle.getString("FECHA")
+            tvDescripcion.text = bundle.getString("DESCRIPCION")
+
+            val imagenId = intent.getIntExtra("IMAGEN", R.drawable.placeholder)
+            ivPortada.setImageResource(obtenerImagenPorId(imagenId))
+
+
+            var youtubeUrl = bundle.getString("YOUTUBEURL")
+
+
+            btnReproducir.setOnClickListener {
+                if (!youtubeUrl.isNullOrEmpty()) {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeUrl))
+                    intent.setPackage("com.google.android.youtube")
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "No hay enlace disponible", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun obtenerImagenPorId(id: Int): Int {
+        return when (id) {
+            1 -> R.drawable.blurryface
+            2 -> R.drawable.trench
+            3 -> R.drawable.vessel
+            4 -> R.drawable.heathens
+            5 -> R.drawable.am
+            6 -> R.drawable.fwn
+            7 -> R.drawable.whatever_people
+            8 -> R.drawable.the_car
+            9 -> R.drawable.demon_days
+            10 -> R.drawable.gorillaz
+            11 -> R.drawable.plastic_beach
+            12 -> R.drawable.cracker_island
+            13 -> R.drawable.room_on_fire
+            14 -> R.drawable.is_this_it
+            15 -> R.drawable.angles
+            16 -> R.drawable.the_new_abnormal
+            17 -> R.drawable.sintesis_o_konor
+            18 -> R.drawable.dia_de_muertos
+            19 -> R.drawable.dinastia_scorpio
+            20 -> R.drawable.humanz
+            else -> R.drawable.placeholder
+        }
+    }
+}
--- app/src/main/java/com/example/tp_apirest/ElementosAdapter.kt
+++ app/src/main/java/com/example/tp_apirest/ElementosAdapter.kt
@@ -2,12 +2,13 @@ package com.example.dezero
 
 import Elemento
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.example.tp_apirest.DetallesActivity
 import com.example.tp_apirest.R
 
 class ElementosAdapter (var elementos: MutableList<Elemento>, var context: Context): RecyclerView.Adapter<ElementosAdapter.ElementoViewHolder>() {
@@ -40,9 +41,20 @@ class ElementosAdapter (var elementos: MutableList<Elemento>, var context: Conte
         holder.txtTitulo.text = item.titulo
         holder.txtAutor.text = item.autor
         holder.txtFecha.text = item.fecha
-        holder.itemView.setOnClickListener(View.OnClickListener{
-            Toast.makeText(context, item.titulo, Toast.LENGTH_SHORT).show() }
-        )
+        holder.itemView.setOnClickListener {
+            val contexto = holder.itemView.context
+            val intent = Intent(contexto, DetallesActivity::class.java)
+
+            intent.putExtra("TITULO", item.titulo)
+            intent.putExtra("AUTOR", item.autor)
+            intent.putExtra("FECHA", item.fecha)
+            intent.putExtra("DESCRIPCION", item.descripcion)
+            intent.putExtra("DURACION", item.duracion)
+            intent.putExtra("IMAGEN", item.imagen)
+            intent.putExtra("YOUTUBEURL", item.youtubeUrl)
+
+            contexto.startActivity(intent)
+        }
     }
 
 }
